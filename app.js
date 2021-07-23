5226ca13f3fefb082d3e5ac749496e92bf753bc5
soumanou abdou zakiou <abdoulaminouz@gmail.com>
2021-07-23 11:49:06 +0100

    heroku commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const userRoutes = require('./routes/user');
 var cors = require('cors');
 
 const uri = "mongodb+srv://zakiou2:eeEAwZPAHqje9iH@cluster0.t6gaj.mongodb.net/test?retryWrites=true&w=majority";
-
 //const uri = "mongodb://localhost:27017";
 const app = express();
 
